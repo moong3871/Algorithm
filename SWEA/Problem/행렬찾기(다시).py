80d803fdb4bf28f10be16b7965ea618aecae6d57
moong3871 <moong3871@gmail.com>
2021-07-20 23:58:32 +0900

    210720

--- SWEA/Problem/행렬찾기(다시).py
+++ SWEA/Problem/행렬찾기(다시).py
@@ -1,22 +1,45 @@
-# 각각의 가로, 세로가 다르다. (행, 렬의 개수가 서로 다르다)
-# #tc 부분행렬 개수 행크기 열크기
-# 크기(행X열) 작은순, 같은 크기이면 행이 작은 순
 def width(i, j):
     cnt = 1
-    dx = j
-    while True:
-        dx += 1
-        if dx >= N:
+    while j < n:
+        j += 1
+        if storage[i][j+1]:
+            cnt += 1
+        else:
             break
-        if
+    return cnt
+
+
+def height(i, j):
+    cnt = 1
+    while j < n:
+        j += 1
+        if storage[i+1][j]:
+            cnt += 1
+        else:
+            break
+    return cnt
+
+
+def reset(ws, hs, w, h):
+    for i in range(ws, w):
+        for j in range(hs, h):
+            storage[i][j] = 0
+    return storage
 
 
 for tc in range(1, int(input())+1):
     n = int(input())
     storage = list(list(map(int, input().split())) for _ in range(n))
-
+    target_list = []
     for i in range(n):
         for j in range(n):
             if storage[i][j]:
                 w = width(i, j)
                 h = height(i, j)
+                target_list.append([w, h, w*h])
+                storage = reset(i, j, i+w, j+h)
+    res = sorted(target_list, key=lambda x: (i[2], i[0]))
+    print('#{} {}'.format(tc, len(res)), end=' ')
+    for i in res:
+        print('{} {}'.format(i[0], i[1]), end=' ')
+    print()
