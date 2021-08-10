f072e3e084f98cb840b97cfe7cc60c12604d457b
moong3871 <moong3871@gmail.com>
2021-08-10 23:49:45 +0900

    210810

---
+++ SWEA/05.Stack2/4875.미로.py
@@ -0,0 +1,35 @@
+# 0 = 통로, 1 = 벽, 2 = 출발, 3 = 도착
+dr = [-1, 0, 0, 1]  # 상 우 하 좌
+dc = [0, 1, -1, 0]  # 상 우 하 좌
+
+
+def check(st, ed):
+    global res
+    sr, sc = st
+    er, ec = ed
+    if maze[sr][sc] == 3:
+        res = 1
+        return
+    maze[sr][sc] = -1
+
+    for mode in range(4):
+        nr = sr + dr[mode]
+        nc = sc + dc[mode]
+        if nr < 0 or nc < 0 or nr >= N or nc >= N:
+            continue
+        if maze[nr][nc] == 0:
+            maze[nr][nc] = -1
+            check(nr, nc)
+
+
+for tc in range(1, int(input()) + 1):
+    N = int(input())
+    maze = [input() for _ in range(N)]
+    res = 0
+    for i in range(N):
+        if '3' in maze[i]:
+            arrive = (i, maze[i].index('3'))
+        if '2' in maze[i]:
+            start = (i, maze[i].index('2'))
+    check(start, arrive)
+    print('#{} {}'.format(tc, res))
